95ad46d038dd5fb1fac4dd99a10678e778673495
Thai Tran <Thai.Tran5656@gmail.com>
2016-10-13 07:50:19 -0700

    node-movie-starter

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.use(bodyParser.json());
 
 app.get('/api/movies', movieCtrl.get);
 app.put('/api/movies', movieCtrl.modify);
+app.get('/api/movies/:movieID', movieCtrl.getByID)
+app.put('/api/movies/:movieID', movieCtrl.modify)
+app.put('/api/movies/:movieID', movieCtrl.delete)
+
 app.post('/api/movies', movieCtrl.add);
 app.delete('/api/movies', movieCtrl.delete);
 
